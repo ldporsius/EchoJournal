03a32c606dd34ea459e845f8e7efb958a582415c
ldporsius <linda.d.porsius@gmail.com>
2025-01-06 16:47:37 +0100

    add sortOption to mood

--- app/src/main/java/nl/codingwithlinda/echojournal/core/domain/model/Mood.kt
+++ app/src/main/java/nl/codingwithlinda/echojournal/core/domain/model/Mood.kt
@@ -1,5 +1,10 @@
 package nl.codingwithlinda.echojournal.core.domain.model
 
-enum class Mood {
-    EXITED , PEACEFUL, NEUTRAL, SAD, STRESSED
+enum class Mood(val sortOrder: Int) {
+    STRESSED(0),
+    SAD(1),
+    NEUTRAL(2),
+    PEACEFUL(3),
+    EXITED(4) ,
+
 }
--- app/src/main/java/nl/codingwithlinda/echojournal/core/presentation/util/BlankMoodsList.kt
+++ app/src/main/java/nl/codingwithlinda/echojournal/core/presentation/util/BlankMoodsList.kt
@@ -3,13 +3,16 @@ package nl.codingwithlinda.echojournal.core.presentation.util
 import nl.codingwithlinda.echojournal.core.domain.model.Mood
 import nl.codingwithlinda.echojournal.feature_entries.presentation.ui_model.UiMood
 
-val blankMoods = Mood.entries.map {
+val blankMoods = Mood.entries
+    .sortedBy {
+        it.sortOrder
+    }
+    .associateWith {
 
-    it to
     UiMood(
         mood = it,
         icon = it.toBlankIcon(),
         color = it.toColor(),
         name = it.toUiText()
     )
-}.toMap()
+}
