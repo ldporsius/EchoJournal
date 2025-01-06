81ff16c4baf7d94b66c652ca627c08562a33ae99
ldporsius <linda.d.porsius@gmail.com>
2025-01-06 16:21:59 +0100

    topic repo test

--- app/src/test/java/nl/codingwithlinda/echojournal/feature_create/data/repo/TopicRepoTest.kt
+++ app/src/test/java/nl/codingwithlinda/echojournal/feature_create/data/repo/TopicRepoTest.kt
@@ -1,28 +1,25 @@
 package nl.codingwithlinda.echojournal.feature_create.data.repo
 
-import nl.codingwithlinda.echojournal.core.data.TopicFactory
-import nl.codingwithlinda.echojournal.core.domain.data_source.repo.TopicsAccess
-import org.junit.Assert.*
-import org.junit.Test
-import app.cash.turbine.test
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import nl.codingwithlinda.echojournal.core.data.TopicFactory
+import nl.codingwithlinda.echojournal.core.domain.data_source.repo.TopicsAccess
 import nl.codingwithlinda.echojournal.test_dispatchers.TestDispatcherProvider
 import org.junit.After
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TopicRepoTest{
-    val dispatcherProvider = TestDispatcherProvider()
-    val topicsAccess = TopicsAccess()
-    val topicFactory = TopicFactory()
-    val repo = TopicRepo(
+    private val dispatcherProvider = TestDispatcherProvider()
+    private val topicsAccess = TopicsAccess()
+    private val topicFactory = TopicFactory()
+    private val repo = TopicRepo(
         topicsAccess,
         topicFactory
     )
@@ -48,30 +45,6 @@ class TopicRepoTest{
             println(it)
         }
 
-        repo.applyFilter.test {
-
-            repo.searchText.update {
-                "a"
-            }
-            val em0 = awaitItem()
-
-            println("em0 = $em0")
-
-            repo.searchText.update {
-                "z"
-            }
-            advanceUntilIdle()
-            val em1 = awaitItem()
-
-            println("em1 = $em1")
-
-            cancelAndConsumeRemainingEvents().also {
-                it.onEach {
-                    println("em2 = $it")
-
-                }
-            }
-        }
     }
 
 }
